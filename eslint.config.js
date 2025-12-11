74f009ed3b9fa935ef3b3b3105d7813b54c6f70b
Adarsh Bansal <adarshbansal1995@gmail.com>
2025-12-11 15:19:12 +0530

    Resolved issues with linting

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,14 @@
 import next from 'eslint-config-next';
+import stylistic from '@stylistic/eslint-plugin';
 
-export default next;
+export default [
+  next,
+  {
+    plugins: {
+      '@stylistic': stylistic,
+    },
+    rules: {
+      '@stylistic/array-element-newline': 'off',
+    },
+  },
+];
