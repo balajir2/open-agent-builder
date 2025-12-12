73165f726cf791e06d1f9646c1695e4b00749bff
Adarsh Bansal <adarshbansal1995@gmail.com>
2025-12-12 12:22:38 +0530

    CI: status check

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,16 @@
 import next from 'eslint-config-next';
 import stylistic from '@stylistic/eslint-plugin';
 
-export default [
-  next,
-  {
-    plugins: {
-      '@stylistic': stylistic,
-    },
-    rules: {
-      '@stylistic/array-element-newline': 'off',
-    },
-  },
-];
+export default next;
+
+// export default [
+//   next,
+//   {
+//     plugins: {
+//       '@stylistic': stylistic,
+//     },
+//     rules: {
+//       '@stylistic/array-element-newline': 'off',
+//     },
+//   },
+// ];
